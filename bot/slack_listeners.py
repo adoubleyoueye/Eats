c17f769364db9fe7a0226650f9356e86ee6fc755
adoubleyoueye <awi.mphaphuli@tuta.io>
2021-08-10 23:42:34 +0200

    Basic slackbot functionality

--- bot/slack_listeners.py
+++ bot/slack_listeners.py
@@ -3,6 +3,7 @@ import os
 
 from slack_bolt import App
 from eats.settings import SLACK_BOT_TOKEN, SLACK_SIGNING_SECRET
+from bot.models import Order
 logger = logging.getLogger(__name__)
 
 # Initialise app with your bot token and signing secret
@@ -17,7 +18,7 @@ app = App(
 # The budget shortcut listens to a shortcut with the callback_id "open_modal"
 
 
-@app.shortcut("budget")
+@app.shortcut("create_order")
 def open_modal(ack, shortcut, client):
     # Acknowledge the shortcut request
     ack()
@@ -25,27 +26,86 @@ def open_modal(ack, shortcut, client):
     client.views_open(
         trigger_id=shortcut["trigger_id"],
         # A simple view payload for a modal
+        # view=SLACK_ORDER_BLOCK
         view={
             "type": "modal",
-            "title": {"type": "plain_text", "text": "My App"},
-            "close": {"type": "plain_text", "text": "Close"},
+            "submit": {
+                "type": "plain_text",
+                "text": "Submit",
+            },
+            "close": {
+                "type": "plain_text",
+                "text": "Cancel"
+            },
+            "title": {
+                "type": "plain_text",
+                "text": "Lunch order"
+            },
             "blocks": [
                 {
                     "type": "section",
                     "text": {
-                        "type": "mrkdwn",
-                        "text": "About the simplest modal you could conceive of :smile:\n\nMaybe <https://api.slack.com/reference/block-kit/interactive-components|*make the modal interactive*> or <https://api.slack.com/surfaces/modals/using#modifying|*learn more advanced modal use cases*>."
+                        "type": "plain_text",
+                        "text": ":wave: Hey!\n\n What are you ordering today?"
                     }
                 },
                 {
-                    "type": "context",
-                    "elements": [
-                        {
-                            "type": "mrkdwn",
-                            "text": "Psssst this modal was designed using <https://api.slack.com/tools/block-kit-builder|*Block Kit Builder*>"
-                        }
-                    ]
+                    "type": "divider"
+                },
+                {
+                    "type": "input",
+                    "element": {
+                        "type": "plain_text_input",
+                        "action_id": "plain_text_input-action"
+                    },
+                    "label": {
+                        "type": "plain_text",
+                        "text": "Item(s)"
+                    }
+                },
+                {
+                    "type": "input",
+                    "element": {
+                        "type": "plain_text_input",
+                        "action_id": "plain_text_input-action"
+                    },
+                    "label": {
+                        "type": "plain_text",
+                        "text": "Price"
+                    }
+                },
+                {
+                    "type": "input",
+                    "element": {
+                        "type": "plain_text_input",
+                        "action_id": "plain_text_input-action"
+                    },
+                    "label": {
+                        "type": "plain_text",
+                        "text": "Restuarant"
+                    }
                 }
             ]
         }
     )
+
+# TODO: santize data
+
+
+@app.view("")
+def handle_view_events(ack, body, logger):
+    ack()
+    response_data = body
+    team_id = response_data['user']['team_id']
+    user_id = response_data['user']['id']
+    form_values = response_data['view']['state']['values']
+    modal_fields = ['item', 'price', 'restaurant']
+    modal_values = []
+    for v in form_values.values():
+        modal_values.append(v['plain_text_input-action']['value'])
+    modal_data = zip(modal_fields, modal_values)
+    try:
+        Order.objects.create(
+            user_id, team_id, item=modal_values[0], price=modal_values[1], restaurant=modal_values[2])
+    except Exception as e:
+        pass  # for now
